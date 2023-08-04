ebc6449474149f5d3b7ca7b836cecbbcb11c10ae
Mateus-Lacerda <mateus.lacerda@discente.ufg.br>
2023-08-04 09:02:21 -0300

    message

--- DDAPI.py
+++ DDAPI.py
@@ -8,7 +8,8 @@ app = Flask(__name__)
 
 port = 8080
 public_url = ngrok.connect(port).public_url
-print(" * ngrok tunnel \"{}\" -> \"http://127.0.0.1:{}\"".format(public_url, port))
+print(f" * ngrok tunnel \"{public_url}\" -> \"http://127.0.0.1:{port}\"")
+app.config["BASE_URL"] = public_url
 
 raw_data = pd.read_csv(r'/home/mateus/Desktop/Estudo/VSCode Projects/DiabetesDetection/diabetes-detection/diabetes-dataset.csv')
 
@@ -21,7 +22,8 @@ X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3)
 user_clf = RandomForestClassifier(max_depth=20, max_features=4, random_state=42)
 user_clf = user_clf.fit(X_train,Y_train)
 
-@app.route("/detect_diabetes", methods=['POST', 'GET'])
+@app.route("/detect_diabetes", methods=[ 'GET'])
+
 def detect_diabetes():
     pregnancies, glucose, blood_pressure, skin_thickness, height, weight, age = request.args.get("pregnancies"), request.args.get("glucose"), request.args.get("blood_pressure"), request.args.get("skin_thickness"), request.args.get("height"), request.args.get("weight"), request.args.get("age")
     data = [
@@ -39,12 +41,12 @@ def detect_diabetes():
     
     probability = user_clf.predict_proba(data)[0][1]
 
-    result = f"De acordo com nossos parâmetros, há uma chance de {probability*100:.2f}% de que você tenha diabetes."
+    result = f"De acordo com nossos parâmetros, há uma chance de {probability*100:.2f}% de que você tenha diabetes. "
     if probability > 0.5:
-        result = (result, "Você deve procurar um médico para realizar exames mais detalhados.")
-    
+        result = (result + "Você deve procurar um médico para realizar exames mais detalhados.")
+    print(result)
 
-    return jsonify({"result":result})
+    return jsonify({"result":result}), 200
 
 
 if __name__ == '__main__':
